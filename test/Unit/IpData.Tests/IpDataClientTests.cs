b7aa1f8db53bea0e5c153370420bffde209cac91
Alex Khil <alex.khil.dev@gmail.com>
2018-12-19 01:57:12 +0200

    Remove unnecessary tests

--- test/Unit/IpData.Tests/IpDataClientTests.cs
+++ test/Unit/IpData.Tests/IpDataClientTests.cs
@@ -31,32 +31,5 @@ namespace IpData.Tests
             // Act/Assert
             sut.ApiKey.Should().Be(apiKey);
         }
-
-        [Theory]
-        [MemberData(nameof(TestData.EmptyOrWhitespaceString), MemberType = typeof(TestData))]
-        public void IpDataClient_WhenCreatedWithInvalidLanguage_ThrowArgumentOutOfRangeException(
-            string culture)
-        {
-            // Arrange/Act
-            Action act = () => new IpDataClient("1", culture);
-
-            // Assert
-            act.Should()
-               .Throw<ArgumentException>()
-               .And.ParamName.Should().Be(nameof(culture));
-        }
-
-        [Theory]
-        [AutoMoqData]
-        public void IpDataClient_WhenCreatedWithLanguage_ReturnedLanguage(
-            string apiKey,
-            string culture)
-        {
-            // Arrange
-            var sut = new IpDataClient(apiKey, culture);
-
-            // Act/Assert
-            sut.Culture.Should().Be(culture);
-        }
     }
 }
